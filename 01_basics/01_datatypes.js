2c0f5654e1db5e4b54ce8ee58fe4476fb80b84e0
Sarfraj Ahmad <125206156+sarfuu4u@users.noreply.github.com>
2023-09-11 10:33:07 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // It is only possible in browser but we are using Node JS
+
+let name = "Sarfraj"
+let age = 20
+let isLoggedIn = true
+
+// number => 2 to power 53
+// string => ""
+// boolean => true/false
+// null => strandalone value
+// undefined =>
+// symbol => for finding unique
+
+// object
+// array
+
+console.log(typeof "Sarfraj")
+console.log(typeof null); // object
