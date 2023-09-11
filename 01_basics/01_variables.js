c2a34a7bb3dd85958ffb2d7aeec3a27194f6631b
Sarfraj Ahmad <125206156+sarfuu4u@users.noreply.github.com>
2023-09-11 10:32:12 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 44
+let accountEmail = "sarfuu@gmail.com"
+var accountPassword = "12345"
+
+// Don't use var because of 
+// issusse in block scope and functional scope
+
+accountCity = "Bhopal"
+let accountState;
+// accountId = 33 // not allowed constant doesn't change
+
+accountEmail="gdki@gmail.com"
+accountPassword="4646546"
+accountCity="Gopalganj"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
