cf941b071e8c4248fa8ae5b373703d05856f399f
Sarfraj Ahmad <125206156+sarfuu4u@users.noreply.github.com>
2023-09-19 00:00:55 +0530

    Add files via upload

---
+++ 01_basics/01_datatypes_summary.js
@@ -0,0 +1,34 @@
+// Primitive
+
+// 7 types: String , Number, null , undefined, Boolean, symbol, BigInt
+
+const score = 123
+const scoreValue = 100.34
+
+const isLogged = true
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')  // both values are not same
+
+const bigNumber = 2142343242442n  // BigInt
+
+
+// reference (Non-primitive)
+
+// Array, Objects, Functions
+
+const heros = ["Shaktiman", "spiderman", "Thor"]
+
+let myObj = {
+    name : "sarfraj",
+    age : 20
+}
+
+const myFunction = function(){
+    console.log("hello world")
+}
+
+console.log(typeof bigNumber)  // BigInt
+console.log(typeof undefined)
+console.log(typeof myObj)
